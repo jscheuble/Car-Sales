bfce863f00cd0d83d6d395ec56ddbf5bad506a35
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-03-17 16:39:09 -0700

    can successfully add and remove features

---
+++ src/actions/featureActions.js
@@ -0,0 +1,11 @@
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+
+export const removeFeature = id => {
+  return { type: REMOVE_FEATURE, payload: id };
+};
+
+export const BUY_ITEM = "BUY_ITEM";
+
+export const buyItem = id => {
+  return { type: BUY_ITEM, payload: id };
+};
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { removeFeature } from "../actions/featureActions";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        onClick={e => {
+          e.preventDefault();
+          props.removeFeature(props.feature.id);
+        }}
+        className="button"
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, { removeFeature })(AddedFeature);
